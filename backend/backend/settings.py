e2119f73021fc5ebc9207caf47a1665a39fa4ae4
yevgeny_novik <novik297443991@yandex.ru>
2025-07-06 12:12:03 +0300

    Попытка поднять таски на локальном работает

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,8 +66,8 @@ DATABASES = {
         # бэкенд postgresql
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'USER': os.getenv('POSTGRES_USER', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password'),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
